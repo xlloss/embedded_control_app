a6d2b808d6d63c6f671c87a3691af92eea7c15f8
Slash.Huang <slash.huang@dfi.com>
2020-05-25 17:31:09 +0800

    fix the function design

--- control_fan/control_fan.c
+++ control_fan/control_fan.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
@@ -9,24 +10,35 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define DEVFILE "/sys/class/pwm/pwmchip1/"
-#define DEVFILE_EXPORT "export"
-#define DEVFILE_UNEXPORT "unexport"
-#define DEVFILE_PWM0 "pwm0"
-#define DEVFILE_PWM0_ENABLE "enable"
-#define DEVFILE_PWM0_PERIOD "period"
-#define DEVFILE_PWM0_DUTY "duty_cycle"
+#define DEVF "/sys/class/pwm/pwmchip"
+#define DEVF_EXPORT "export"
+#define DEVF_UNEXPORT "unexport"
+#define DEVF_PWM_ENABLE "enable"
+#define DEVF_PWM_PERIOD "period"
+#define DEVF_PWM_DUTY "duty_cycle"
 
-//#define DEVFILE "/sys/class/pwm/pwmchip1/npwm"
+struct sys_pwm {
+	int port;
+	int channel;
+	int enable;
+	char *con_buf;
+};
 
-int pwm_chan_export(char *control_buf)
+int pwm_chan_export(struct sys_pwm *pwm)
 {
     int fd;
     char writebuf[5];
     int w_len;
+    int pwm_channel, pwm_port;
+    char *conbuf;
 
-    sprintf(control_buf, "%s%s%s", DEVFILE, DEVFILE_PWM0, DEVFILE_PWM0_DUTY);
-    fd = open(control_buf, O_WRONLY);
+    conbuf = pwm->con_buf;
+    pwm_channel = pwm->channel;
+    pwm_port = pwm->port;
+    sprintf(conbuf, "%s/%d/%d/%s", DEVF,
+        pwm_port, pwm_channel, DEVF_EXPORT);
+
+    fd = open(conbuf, O_WRONLY);
     if (fd < 0) {
         perror("open");
         return 0;
@@ -40,14 +52,21 @@ int pwm_chan_export(char *control_buf)
     close(fd);
 }
 
-int pwm_chan_unexport(char *control_buf)
+int pwm_chan_unexport(struct sys_pwm *pwm)
 {
     int fd;
     char writebuf[5];
     int w_len;
+    int pwm_channel, pwm_port;
+    char *conbuf;
 
-    sprintf(control_buf, "%s%s%s", DEVFILE, DEVFILE_PWM0, DEVFILE_UNEXPORT);
-    fd = open(control_buf, O_WRONLY);
+    conbuf = pwm->con_buf;
+    pwm_channel = pwm->channel;
+    pwm_port = pwm->port;
+    sprintf(conbuf, "%s/%d/%d/%s", DEVF,
+        pwm_port, pwm_channel, DEVF_UNEXPORT);
+
+    fd = open(conbuf, O_WRONLY);
     if (fd < 0) {
         perror("open");
         return 0;
@@ -61,20 +80,27 @@ int pwm_chan_unexport(char *control_buf)
     close(fd);
 }
 
-int pwm_set_duty(char *control_buf, int duty_time)
+int pwm_set_duty(struct sys_pwm *pwm, int duty_time)
 {
     int fd;
     char writebuf[20];
     int w_len;
+    int pwm_channel, pwm_port;
+    char *conbuf;
 
-    sprintf(control_buf, "%s%s", DEVFILE, DEVFILE_PWM0_DUTY);
-    fd = open(control_buf, O_WRONLY);
+    conbuf = pwm->con_buf;
+    pwm_channel = pwm->channel;
+    pwm_port = pwm->port;
+    sprintf(conbuf, "%s/%d/%d/%s", DEVF,
+        pwm_port, pwm_channel, DEVF_PWM_DUTY);
+
+    fd = open(conbuf, O_WRONLY);
     if (fd < 0) {
         perror("open");
         return 0;
     }
 
-    sprintf(writebuf, "%s", duty_time);
+    sprintf(writebuf, "%d", duty_time);
     w_len = write(fd, writebuf, strlen(writebuf));
     if (w_len < 0)
         perror("write");
@@ -82,31 +108,69 @@ int pwm_set_duty(char *control_buf, int duty_time)
     close(fd);
 }
 
-int pwm_set_period(char *control_buf, int period_time)
+int pwm_set_period(struct sys_pwm *pwm, int period_time)
 {
     int fd;
     char writebuf[20];
     int w_len;
+    int pwm_channel, pwm_port;
+    char *conbuf;
 
-    sprintf(control_buf, "%s%s", DEVFILE, DEVFILE_PWM0_PERIOD);
-    fd = open(control_buf, O_WRONLY);
+    conbuf = pwm->con_buf;
+    pwm_channel = pwm->channel;
+    pwm_port = pwm->port;
+    sprintf(conbuf, "%s/%d/%d/%s", DEVF,
+        pwm_port, pwm_channel, DEVF_PWM_PERIOD);
+
+    fd = open(conbuf, O_WRONLY);
     if (fd < 0) {
         perror("open");
         return 0;
     }
 
-    sprintf(writebuf, "%s", duty_time);
+    sprintf(writebuf, "%d", period_time);
     w_len = write(fd, writebuf, strlen(writebuf));
     if (w_len < 0)
         perror("write");
 
     close(fd);
 }
+
+int pwm_set_enable(struct sys_pwm *pwm)
+{
+    int fd;
+    char writebuf[20];
+    int w_len;
+    int pwm_channel, pwm_port, enable;
+    char *conbuf;
+
+    conbuf = pwm->con_buf;
+    pwm_channel = pwm->channel;
+    pwm_port = pwm->port;
+
+    sprintf(conbuf, "%s/%d/%d/%s", DEVF,
+        pwm_port, pwm_channel, DEVF_PWM_ENABLE);
+
+    fd = open(conbuf, O_WRONLY);
+    if (fd < 0) {
+        perror("open");
+        return 0;
+    }
+
+    enable = pwm->enable;
+    sprintf(writebuf, "%d", enable);
+    w_len = write(fd, writebuf, strlen(writebuf));
+    if (w_len < 0)
+        perror("write");
+
+    close(fd);
+}
+
 
 int main(int argc, char** argv)
 {
     int fd;
-    int led, value;
+    int port, duty, channel, period;
     int opts;
     int option_index = 0;
     char *pwm_ch = "0\r\n";
@@ -115,45 +179,48 @@ int main(int argc, char** argv)
     char conbuf[50];
     int w_len;
     int i;
-//    /* getopt_long stores the option index here. */
-//    struct option long_options[] = {
-//        {"led", required_argument, 0, 'l'},
-//        {"value", required_argument, 0, 'v'},
-//        {"?",  no_argument, 0, '?'},
-//        {0, 0, 0, 0}
-//    };
-//
-//    while (1) {
-//        opts = getopt_long (argc, argv,
-//        "l:v:", long_options, &option_index);
-//
-//        /* Detect the end of the options. */
-//        if (opts == -1)
-//            break;
-//
-//        switch (opts) {
-//        case 'l':
-//            led = atoi(optarg);
-//            break;
-//
-//        case 'v':
-//            value = atoi(optarg);
-//            break;
-//
-//        case '?':
-//            printf(
-//                "./test_led_char_driver -l LED_NUM -v ON/OFF\r\n"
-//                "LED_NUM : 0 - 3\r\n"
-//                "ON/OFF :\r\n"
-//                "   ON : 1\r\n"
-//                "   OFF: 0\r\n"
-//                "./test_led_char_driver -l 0 -v 1\r\n : led 0 on\r\n"
-//                "./test_led_char_driver -l 0 -v 0\r\n : led 0 off\r\n");
-//            break;
-//        default:
-//                break;
-//        }
-//    }
+
+    /* getopt_long stores the option index here. */
+    struct option long_options[] = {
+        {"period", required_argument, 0, 'p'},
+        {"duty", required_argument, 0, 'd'},
+        {"port", required_argument, 0, 'o'},
+        {"channel", required_argument, 0, 'c'},
+        {"?",  no_argument, 0, '?'},
+        {0, 0, 0, 0}
+    };
+
+    while (1) {
+        opts = getopt_long (argc, argv,
+        "p:d:o:c:", long_options, &option_index);
+
+        /* Detect the end of the options. */
+        if (opts == -1)
+            break;
+
+        switch (opts) {
+        case 'p':
+            period = atoi(optarg);
+            break;
+
+        case 'd':
+            duty = atoi(optarg);
+            break;
+
+        case 'o':
+            port = atoi(optarg);
+            break;
+
+        case 'c':
+            channel = atoi(optarg);
+            break;
+        case '?':
+            printf("help");
+            break;
+        default:
+                break;
+        }
+    }
 
     return 0;
 }
